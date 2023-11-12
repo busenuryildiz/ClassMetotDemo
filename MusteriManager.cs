bbc8a0c588f08e3e4b4bcbbc903e52dc11b62f0f
busenuryildiz <busenuryildiz5337@gmail.com>
2023-11-12 13:11:31 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + "Numaralı müşteri" + " " + musteri.Adi + " " + musteri.Soyadi + " Listeye eklendi");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + "Numaralı müşteri" + " " + musteri.Adi + " " + musteri.Soyadi + " Listeden silindi");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + "Numaralı müşteri" + " " + musteri.Adi + " " + musteri.Soyadi + " Listelendi");
+            Console.WriteLine("Müşteri ID: " + musteri.Id );
+            Console.WriteLine("Müşteri Adı Soyadı: " + musteri.Adi+" "+musteri.Soyadi);
+            Console.WriteLine("Müşteri Yaşı: " + musteri.Yas);
+            Console.WriteLine("Kredi Notu: " + musteri.KrediNotu);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Buse";
+            musteri1.Soyadi = "Yıldız";
+            musteri1.Yas = 23;
+            musteri1.KrediNotu = 82;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Burak";
+            musteri2.Soyadi = "Yıldız";
+            musteri2.Yas = 27;
+            musteri2.KrediNotu = 58;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Belinay";
+            musteri3.Soyadi = "Yıldız";
+            musteri3.Yas = 19;
+            musteri3.KrediNotu = 79;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri ID: " + musteri.Id);
+                Console.WriteLine("Müşteri Adı: " + musteri.Adi);
+                Console.WriteLine("Müşteri Soyadı: " + musteri.Soyadi);
+                Console.WriteLine("Müşteri Yaşı: " + musteri.Yas);
+                Console.WriteLine("Müşteri Kredi Notu: " + musteri.KrediNotu);
+                Console.WriteLine("-----------------------------------------");
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri1);
+            Console.WriteLine("-----------------------------------------");
+            musteriManager.Listele(musteri1);
+
+        }
+    }
+}
